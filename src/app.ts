39274714600642c99143e0523ca389375522556f
222112038 <falanarofako29@gmail.com>
2024-07-25 14:24:55 +0700

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import express from 'express';
-import morgan from 'morgan';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import connectDB from './config/db';
-import rtRoutes from './routes/rtRoutes';
-import rumahTanggaRoutes from './routes/rumahTanggaRoutes';
-import authRoutes from './routes/authRoutes';
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import connectDB from "./config/db";
+import rtRoutes from "./routes/rtRoutes";
+import rumahTanggaRoutes from "./routes/rumahTanggaRoutes";
+import authRoutes from "./routes/authRoutes";
+
+const cors = require("cors");
 
 dotenv.config();
 
@@ -13,12 +15,19 @@ const app = express();
 
 connectDB();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
 app.use(express.json());
 // app.use(bodyParser.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/rumahTangga', rumahTanggaRoutes);
-app.use('/api/rt', rtRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/rumahTangga", rumahTanggaRoutes);
+app.use("/api/rt", rtRoutes);
 
 export default app;
