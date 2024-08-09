43d7c97fdf1b80999c761194e1347e965676c82e
222112038 <falanarofako29@gmail.com>
2024-08-09 07:48:36 +0700

    configure cors 2

--- src/app.ts
+++ src/app.ts
@@ -25,8 +25,8 @@ app.use(morgan("dev"));
 
 app.use(cors({
   origin: 'https://desa-cantik-sda.vercel.app', // Ganti dengan domain Anda
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metode yang diizinkan
-  allowedHeaders: ['Content-Type', 'Authorization']
+  // methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metode yang diizinkan
+  // allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
